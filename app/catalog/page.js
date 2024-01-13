ba0fd6c0a337f2bdc5fc9a04e58cbf18a28d4b3a
Furkan Kartal <furkankartalus@gmail.com>
2024-01-13 16:54:04 +0100

    Add more tabs

--- app/catalog/page.js
+++ app/catalog/page.js
@@ -1,18 +1,26 @@
 'use client';
 import { Carousel } from 'flowbite-react';
+import { useSearchParams } from 'next/navigation';
 import React from 'react';
 // import Carousel from '../components/Carousel';
 
 const page = () => {
-	const [filter, setFilter] = React.useState('urunler');
+	const [filter, setFilter] = React.useState('ostenitik');
+	const searchParams = useSearchParams();
+	const tab = searchParams.get('tab');
+
+	React.useEffect(() => {
+		if (tab) {
+			setFilter(tab);
+		}
+	}, [tab]);
 
 	return (
 		<div className='px-0 flex justify-center  bg-cover bg-center bg-no-repeat bg-[url(/landing_bg.webp)]'>
 			{/* <Carousel /> */}
 			<div className='w-full md:min-w-fit flex flex-col gap-8 md:gap-16 items-center py-8 bg-customNavy/70 backdrop-blur-sm '>
-				<div className='flex flex-col items-center justify-center w-full pt-24'>
-					<div className='flex flex-row items-center justify-center w-full'>
-						<button
+				<div className='flex flex-row items-center justify-start md:justify-center px-12 w-11/12  overflow-x-scroll overflow-y-visible pt-24 *:h-16 md:*:h-auto md:gap-0 gap-1 '>
+					{/* <button
 							className={`${
 								filter !== 'all'
 									? 'bg-customWhite opacity-60 text-customPrimary/80'
@@ -21,47 +29,114 @@ const page = () => {
 							onClick={() => setFilter('all')}
 						>
 							Tümü
-						</button>
-						<button
-							className={`${
-								filter !== 'hakkimizda'
-									? 'bg-customWhite opacity-60 text-customPrimary/80'
-									: 'bg-customWhite text-customPrimary/100'
-							} text-customPrimary/60 font-bold py-2 px-4 cursor-pointer`}
-							onClick={() => setFilter('hakkimizda')}
-						>
-							Hakkımızda
-						</button>
-						<button
-							className={`${
-								filter !== 'urunler'
-									? 'bg-customWhite opacity-60 text-customPrimary/80'
-									: 'bg-customWhite text-customPrimary/100'
-							}  font-bold py-2 px-4 rounded-r cursor-pointer`}
-							onClick={() => setFilter('urunler')}
-						>
-							Ürünler
-						</button>
-					</div>
-					<div className='w-full h-full'></div>
+						</button> */}
+					<button
+						className={`${
+							filter !== 'ostenitik'
+								? 'bg-customWhite opacity-60 text-customPrimary/80'
+								: 'bg-customWhite text-customPrimary/100'
+						}  font-bold py-2 px-4 rounded-l cursor-pointer`}
+						onClick={() => setFilter('ostenitik')}
+					>
+						Östenitik
+					</button>
+					<button
+						className={`${
+							filter !== 'ferritik'
+								? 'bg-customWhite opacity-60 text-customPrimary/80'
+								: 'bg-customWhite text-customPrimary/100'
+						}  font-bold py-2 px-4  cursor-pointer`}
+						onClick={() => setFilter('ferritik')}
+					>
+						Ferritik
+					</button>
+					<button
+						className={`${
+							filter !== 'angle-bar'
+								? 'bg-customWhite opacity-60 text-customPrimary/80'
+								: 'bg-customWhite text-customPrimary/100'
+						}  font-bold py-2 px-4  cursor-pointer`}
+						onClick={() => setFilter('angle-bar')}
+					>
+						Angle Bar
+					</button>
+					<button
+						className={`${
+							filter !== 'paslanmaz-lama'
+								? 'bg-customWhite opacity-60 text-customPrimary/80'
+								: 'bg-customWhite text-customPrimary/100'
+						}  font-bold py-2 px-4  cursor-pointer`}
+						onClick={() => setFilter('paslanmaz-lama')}
+					>
+						Paslanmaz Lama
+					</button>
+					<button
+						className={`${
+							filter !== 'round-bar'
+								? 'bg-customWhite opacity-60 text-customPrimary/80'
+								: 'bg-customWhite text-customPrimary/100'
+						}  font-bold py-2 px-4  cursor-pointer`}
+						onClick={() => setFilter('round-bar')}
+					>
+						Round Bar
+					</button>
+					<button
+						className={`${
+							filter !== 'hakkimizda'
+								? 'bg-customWhite opacity-60 text-customPrimary/80'
+								: 'bg-customWhite text-customPrimary/100'
+						} text-customPrimary/60 font-bold rounded-r py-2 px-4 cursor-pointer`}
+						onClick={() => setFilter('hakkimizda')}
+					>
+						Hakkımızda
+					</button>
 				</div>
-
+				{/* <div className='w-full h-full'></div> */}
+				{/* 
 				{filter === 'all' && (
 					<Carousel slide={false} className='h-screen pb-16' indicators={false}>
-						<Hakkimizda />
-						{Urunler.map((item, i) => {
-							return React.cloneElement(item, { key: 'f' + i });
-						})}
+					<Hakkimizda />
+					{Ostenitik.map((item, i) => {
+						return React.cloneElement(item, { key: 'f' + i });
+					})}
 					</Carousel>
-				)}
+				)} */}
 				{filter === 'hakkimizda' && (
 					<Carousel slide={false} className='h-screen pb-16' indicators={false}>
 						{<Hakkimizda />}
 					</Carousel>
 				)}
-				{filter === 'urunler' && (
+				{filter === 'ostenitik' && (
 					<Carousel slide={false} className='h-screen pb-16' indicators={false}>
-						{Urunler.map((item, i) => {
+						{Ostenitik.map((item, i) => {
+							return React.cloneElement(item, { key: 'o' + i });
+						})}
+					</Carousel>
+				)}
+				{filter === 'ferritik' && (
+					<Carousel slide={false} className='h-screen pb-16' indicators={false}>
+						{Ferritik.map((item, i) => {
+							return React.cloneElement(item, { key: 'f' + i });
+						})}
+					</Carousel>
+				)}
+				{filter === 'angle-bar' && (
+					<Carousel slide={false} className='h-screen pb-16' indicators={false}>
+						{AngleBar.map((item, i) => {
+							return React.cloneElement(item, { key: 'f' + i });
+						})}
+					</Carousel>
+				)}
+				{filter === 'round-bar' && (
+					<Carousel slide={false} className='h-screen pb-16' indicators={false}>
+						{RoundBar.map((item, i) => {
+							return React.cloneElement(item, { key: 'f' + i });
+						})}
+					</Carousel>
+				)}
+				{filter === 'paslanmaz-lama' && (
+					<Carousel slide={false} className='h-screen pb-16' indicators={false}>
+						{PaslanmazLama.map((item, i) => {
 							return React.cloneElement(item, { key: 'f' + i });
 						})}
 					</Carousel>
@@ -79,12 +154,18 @@ const Hakkimizda = () => (
 	/>
 );
 
-const Urunler = [
+const Ostenitik = [
 	<img
 		src='/katalog/o-ana@2x.png'
 		alt='...'
 		className='w-full h-full select-none object-contain'
 	/>,
+	<img
+		src='/katalog/Östenitik@2x.png'
+		alt='...'
+		className='w-full h-full select-none object-contain'
+	/>,
+
 	<img
 		src='/katalog/o-1@2x.png'
 		alt='...'
@@ -140,8 +221,16 @@ const Urunler = [
 		alt='...'
 		className='w-full h-full select-none object-contain'
 	/>,
+];
+
+const Ferritik = [
 	<img
 		src='/katalog/f-ana@2x.png'
+		alt='...'
+		className='w-full h-full select-none object-contain'
+	/>,
+	<img
+		src='/katalog/tablo-1.png'
 		alt='...'
 		className='w-full h-full select-none object-contain'
 	/>,
@@ -170,30 +259,30 @@ const Urunler = [
 		alt='...'
 		className='w-full h-full select-none object-contain'
 	/>,
-	<img
-		src='/katalog/tablo-1.png'
-		alt='...'
-		className='w-full h-full select-none object-contain'
-	/>,
-	<img
-		src='/katalog/Östenitik@2x.png'
-		alt='...'
-		className='w-full h-full select-none object-contain'
-	/>,
-	<img
-		src='/katalog/Paslanmaz Lama@2x.png'
-		alt='...'
-		className='w-full h-full select-none object-contain'
-	/>,
-	<img
-		src='/katalog/Round Bar@2x.png'
-		alt='...'
-		className='w-full h-full select-none object-contain'
-	/>,
+];
+
+const AngleBar = [
 	<img
 		src='/katalog/Angle Bar@2x.png'
 		alt='...'
 		className='w-full h-full select-none object-contain'
 	/>,
 ];
+
+const RoundBar = [
+	<img
+		src='/katalog/Round Bar@2x.png'
+		alt='...'
+		className='w-full h-full select-none object-contain'
+	/>,
+];
+
+const PaslanmazLama = [
+	<img
+		src='/katalog/Paslanmaz Lama@2x.png'
+		alt='...'
+		className='w-full h-full select-none object-contain'
+	/>,
+];
+
 export default page;
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,124 +7,145 @@ import React from 'react';
 const font = Didact_Gothic({ subsets: ['latin'], weight: '400' });
 
 const Navbar = () => {
-  const [menuOpened, setMenuOpened] = React.useState(false);
-  const [scrolled, setScrolled] = React.useState(false);
-  if (typeof window !== 'undefined') {
-    window.addEventListener('scroll', () => setMenuOpened(false));
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 60) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    });
-  }
+	const [menuOpened, setMenuOpened] = React.useState(false);
+	const [scrolled, setScrolled] = React.useState(false);
+	if (typeof window !== 'undefined') {
+		window.addEventListener('scroll', () => setMenuOpened(false));
+		window.addEventListener('scroll', () => {
+			if (window.scrollY > 60) {
+				setScrolled(true);
+			} else {
+				setScrolled(false);
+			}
+		});
+	}
 
-  const scrollToDiv = (id) => {
-    let navHeight = 80;
-    if (window.innerWidth >= 1024) {
-      navHeight = 96;
-    }
+	const scrollToDiv = id => {
+		let navHeight = 80;
+		if (window.innerWidth >= 1024) {
+			navHeight = 96;
+		}
 
-    window.scrollTo({
-      top: document.querySelector(id).offsetTop - navHeight,
-      behavior: 'smooth',
-    });
-  };
+		window.scrollTo({
+			top: document.querySelector(id).offsetTop - navHeight,
+			behavior: 'smooth',
+		});
+	};
 
-  return (
-    <nav
-      className={`${
-        scrolled ? 'bg-customWhite text-customPrimary' : 'text-customWhite ' && menuOpened ? 'bg-customWhite text-customPrimary' : 'text-customWhite'
-      } fixed w-screen z-[1000] transition-all duration-500 pl-0 lg:pl-20 lg:py-2 px-4 lg:px-20  select-none  border-b-[1px] border-white/30`}
-    >
-      <div className='flex items-center justify-between'>
-        <Link
-          href='/'
-          className='flex items-center md:text-xl lg:text-3xl gap-6 p-2 md:p-0'
-        >
-          <img src='/ysll.webp' className='h-14 md:h-20' alt='ysll' />
-          <h2 className={font.className + " tracking-wider text-base  md:text-2xl leading-none"}>YEŞİLBURSA <br className='md:hidden block' /> PASLANMAZ</h2>
-        </Link>
-        <ul className='sm:flex justify-between hidden gap-4'>
-          {/* <li className='cursor-pointer' onClick={() => scrollToDiv('#about')}> */}
-          <Link href='/catalog' className='cursor-pointer'>
-            Hakkımızda
-            </Link>
-          {/* </li> */}
-          <Link href='/catalog' className='cursor-pointer'>
-            Katalog
-          </Link>
-          <li className='cursor-pointer'>Üretim</li>
-          <li
-            className='cursor-pointer'
-            onClick={() => scrollToDiv('#contact')}
-          >
-            İletişim
-          </li>
-        </ul>
-        <svg
-          onClick={() => setMenuOpened(!menuOpened)}
-          className={`overflow-visible sm:hidden mr-2 ${(menuOpened || scrolled) ? 'stroke-customPrimary' : 'stroke-customWhite'}`}
-          width='28'
-          height='16'
-          viewBox='0 0 28 16'
-          fill='none'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <line
-            className={`${
-              menuOpened ? 'rotate-45 -translate-y-[0.3rem]' : null
-            } origin-left duration-500 transition-all`}
-            y1='1'
-            x2='28'
-            y2='1'
-            // stroke='#EADAFF'
-            strokeWidth='2'
-          />
-          <line
-            className={`${
-              menuOpened ? 'opacity-0' : null
-            } duration-300 transition-all`}
-            y1='8'
-            x2='28'
-            y2='8'
-            // stroke='#EADAFF'
-            strokeWidth='2'
-          />
-          <line
-            className={`${
-              menuOpened ? '-rotate-45 translate-y-[0.3rem]' : null
-            } origin-left duration-500 transition-all`}
-            y1='15'
-            x2='28'
-            y2='15'
-            // stroke='#EADAFF'
-            strokeWidth='2'
-          />
-        </svg>
-      </div>
-      <ul
-        className={`${
-          menuOpened ? 'h-60' : 'opacity-0 h-0'
-        } flex flex-col gap-4 transition-all overflow-hidden sm:hidden items-center bg-customWhite/100 text-customPrimary absolute w-full top-32 left-0 text-md`}
-      >
-        <li className='cursor-pointer' onClick={() => scrollToDiv('#about')}>
+	return (
+		<nav
+			className={`${
+				scrolled
+					? 'bg-customWhite text-customPrimary'
+					: 'text-customWhite ' && menuOpened
+					? 'bg-customWhite text-customPrimary'
+					: 'text-customWhite'
+			} fixed w-screen z-[1000] transition-all duration-500 pl-0 lg:pl-20 lg:py-2 px-4 lg:px-20  select-none  border-b-[1px] border-white/30`}
+		>
+			<div className='flex items-center justify-between'>
+				<Link
+					href='/'
+					className='flex items-center md:text-xl lg:text-3xl gap-6 p-2 md:p-0'
+				>
+					<img src='/ysll.webp' className='h-14 md:h-20' alt='ysll' />
+					<h2
+						className={
+							font.className +
+							' tracking-wider text-base  md:text-2xl leading-none'
+						}
+					>
+						YEŞİLBURSA <br className='md:hidden block' /> PASLANMAZ
+					</h2>
+				</Link>
+				<ul className='sm:flex justify-between hidden gap-4'>
+					{/* <li className='cursor-pointer' onClick={() => scrollToDiv('#about')}> */}
+					<Link href='/catalog?tab=hakkimizda' className='cursor-pointer'>
+						Hakkımızda
+					</Link>
+					{/* </li> */}
+					<Link href='/catalog?tab=ostenitik' className='cursor-pointer'>
+						Ürünler
+					</Link>
+					<li
+						className='cursor-pointer'
+						onClick={() => scrollToDiv('#contact')}
+					>
+						İletişim
+					</li>
+				</ul>
+				<svg
+					onClick={() => setMenuOpened(!menuOpened)}
+					className={`overflow-visible sm:hidden mr-2 ${
+						menuOpened || scrolled
+							? 'stroke-customPrimary'
+							: 'stroke-customWhite'
+					}`}
+					width='28'
+					height='16'
+					viewBox='0 0 28 16'
+					fill='none'
+					xmlns='http://www.w3.org/2000/svg'
+				>
+					<line
+						className={`${
+							menuOpened ? 'rotate-45 -translate-y-[0.3rem]' : null
+						} origin-left duration-500 transition-all`}
+						y1='1'
+						x2='28'
+						y2='1'
+						// stroke='#EADAFF'
+						strokeWidth='2'
+					/>
+					<line
+						className={`${
+							menuOpened ? 'opacity-0' : null
+						} duration-300 transition-all`}
+						y1='8'
+						x2='28'
+						y2='8'
+						// stroke='#EADAFF'
+						strokeWidth='2'
+					/>
+					<line
+						className={`${
+							menuOpened ? '-rotate-45 translate-y-[0.3rem]' : null
+						} origin-left duration-500 transition-all`}
+						y1='15'
+						x2='28'
+						y2='15'
+						// stroke='#EADAFF'
+						strokeWidth='2'
+					/>
+				</svg>
+			</div>
+			<ul
+				className={`${
+					menuOpened ? 'h-60' : 'opacity-0 h-0'
+				} flex flex-col gap-4 transition-all overflow-hidden sm:hidden items-center bg-customWhite/100 text-customPrimary absolute w-full top-18 left-0 text-md`}
+			>
+				{/* <li className='cursor-pointer' onClick={() => scrollToDiv('#about')}>
           Hakkımızda
-        </li>
-        <li className='cursor-pointer' onClick={() => scrollToDiv('#products')}>
-          Ürünler
-        </li>
-        <li className='cursor-pointer'>Üretim</li>
-        <li className='cursor-pointer' onClick={() => scrollToDiv('#features')}>
-          Özellikler
-        </li>
-        <li className='cursor-pointer' onClick={() => scrollToDiv('#contact')}>
-          İletişim
-        </li>
-      </ul>
-    </nav>
-  );
+        </li> */}
+				<Link
+					href='/catalog?tab=hakkimizda'
+					className='cursor-pointer mt-12'
+					onClick={() => setMenuOpened(false)}
+				>
+					Hakkımızda
+				</Link>
+				<Link
+					href='/catalog?tab=ostenitik'
+					className='cursor-pointer'
+					onClick={() => setMenuOpened(false)}
+				>
+					Ürünler
+				</Link>
+				<li className='cursor-pointer' onClick={() => scrollToDiv('#contact')}>
+					İletişim
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;
